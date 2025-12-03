e3cc688ca857faf1dd7e2b48fac400a25b86776c
Rodion <tridvad1@gmail.com>
2025-12-03 23:13:18 +0300

    feat: update nvim and alacritty

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,5 +1,8 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  priority = 1000
+  "craftzdog/solarized-osaka.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {
+    transparent = true,
+  },
 }
